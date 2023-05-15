93f2334bd6e2961604ec3d9d875d758bf72ec7a7
Juan David Saraza Nieto <juand.sarazan@uqvirtual.edu.co>
2023-05-15 16:41:32 -0500

    some changes

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,27 +1,21 @@
 //Invocamos a la conexion de la DB
 const conexion = require('../database/db');
+
 //add a new movie
 exports.save = (req, res)=>{
-    const movie_name = req.body.movie_name;
-    const movie_year = req.body.movie_year;
-    const movie_director = req.body.movie_director;
-    const movie_duration =req.body.movie_duration;
-    const movie_genre = req.body.movie_genre;
-    const movie_language = req.body.movie_language;
+    const { movie_name, movie_year, movie_director, movie_duration, movie_genre, movie_language } = req.body;
     
-    conexion.query('INSERT INTO movies SET ?',{movie_name:movie_name,movie_year:movie_year, movie_duration:movie_duration,
-        movie_director:movie_director, movie_genre:movie_genre, movie_language:movie_language}, (error, results)=>{
+    conexion.query('INSERT INTO movies SET ?', { movie_name, movie_year, movie_director, movie_duration, movie_genre, movie_language }, (error, results)=>{
         if(error){
             console.log(error);
         }else{
-            //console.log(results);   
-            res.redirect('/');         
+            res.redirect('/');
         }
-});
-
+    });
 };
+
 //update the selected movie
-exports.update = (req, res)=>{
+exports.update = async (req, res)=>{
     const movie_id = req.body.movie_id;
     const movie_name = req.body.movie_name;
     const movie_duration =req.body.movie_duration;
@@ -30,12 +24,12 @@ exports.update = (req, res)=>{
     const movie_language = req.body.movie_language;
     const movie_year = req.body.movie_year;
     
-    conexion.query('UPDATE movies SET ? WHERE movie_id = ?',[{movie_id:movie_id, movie_name:movie_name, movie_duration:movie_duration, movie_director:movie_director,  
-        movie_genre:movie_genre, movie_language:movie_language, movie_year:movie_year}, movie_id], (error, results)=>{
-        if(error){
-            console.log(error);
-        }else{           
-            res.redirect('/');         
-        }
-    });
+    try {
+        const sql = 'UPDATE movies SET movie_id = ?, movie_name = ?, movie_duration = ?, movie_director = ?, movie_genre = ?, movie_language = ?, movie_year = ? WHERE movie_id = ?';
+        const values = [movie_id, movie_name, movie_duration, movie_director, movie_genre, movie_language, movie_year, movie_id];
+        await conexion.query(sql, values);
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 };
--- db.sql
+++ db.sql
@@ -24,4 +24,26 @@ CREATE TABLE favorites (
 );
 
 SHOW TABLES;
-DESC 
+
+##Inserts, dataset
+
+INSERT INTO movies (movie_name, movie_duration, movie_director, movie_genre, movie_language, movie_year) VALUES
+('Pulp Fiction', 154, 'Quentin Tarantino', 'Crime, Drama', 'English', 1994),
+('Forrest Gump', 142, 'Robert Zemeckis', 'Drama, Romance', 'English', 1994),
+('The Shawshank Redemption', 142, 'Frank Darabont', 'Drama', 'English', 1994),
+('The Lion King', 89, 'Roger Allers, Rob Minkoff', 'Animation, Adventure, Drama', 'English', 1994),
+('Toy Story', 81, 'John Lasseter', 'Animation, Adventure, Comedy', 'English', 1995),
+('Braveheart', 178, 'Mel Gibson', 'Action, Biography, Drama', 'English', 1995),
+('The Usual Suspects', 106, 'Bryan Singer', 'Crime, Mystery, Thriller', 'English', 1995),
+('Heat', 170, 'Michael Mann', 'Action, Crime, Drama', 'English', 1995),
+('Independence Day', 145, 'Roland Emmerich', 'Action, Adventure, Sci-Fi', 'English', 1996),
+('Fargo', 98, 'Joel Coen, Ethan Coen', 'Crime, Drama, Thriller', 'English', 1996),
+('Good Will Hunting', 126, 'Gus Van Sant', 'Drama, Romance', 'English', 1997),
+('Titanic', 194, 'James Cameron', 'Drama, Romance', 'English', 1997),
+('Saving Private Ryan', 169, 'Steven Spielberg', 'Drama, War', 'English', 1998),
+('The Truman Show', 103, 'Peter Weir', 'Comedy, Drama, Sci-Fi', 'English', 1998),
+('The Sixth Sense', 107, 'M. Night Shyamalan', 'Drama, Mystery, Thriller', 'English', 1999),
+('American Beauty', 122, 'Sam Mendes', 'Drama, Romance', 'English', 1999),
+('Gladiator', 155, 'Ridley Scott', 'Action, Adventure, Drama', 'English', 2000),
+('Memento', 113, 'Christopher Nolan', 'Mystery, Thriller', 'English', 2000),
+('The Lord of the Rings: The Fellowship of the Ring', 'English', 2006);
